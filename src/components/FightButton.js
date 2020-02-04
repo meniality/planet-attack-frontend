c7f3eb715277034b209c397371c286bd11e304d6
meniality <patrh_99@hotmail.com>
2020-02-04 13:39:40 -0700

    added fight button

---
+++ src/components/FightButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HandleMath from './HandleMath'
+
+export default function FightButton(props){
+  return (
+    <div>
+      {/* <button onClick ={<HandleMath {...props}/>}>FIGHT!</button> */}
+    </div>
+  )
+}
